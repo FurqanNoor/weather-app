cea2db9db551e588282dc7eef6db479527b17318
FurqanNoor <82640979+FurqanNoor@users.noreply.github.com>
2024-12-17 18:44:39 +0200

    code base

---
+++ src/app/layout.tsx
@@ -0,0 +1,68 @@
+
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { siteConfig } from "@/lib/site-config";
+
+// Import and configure the Inter font
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: ["Next.js", "React", "Tailwind CSS", "Framer Motion"],
+  authors: [
+    {
+      name: "Furqan Noor",
+      url: siteConfig.url,
+    },
+  ],
+  creator: "Furqan",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: siteConfig.twitterHandle,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.variable} container max-w-2xl mx-auto min-h-screen flex flex-col px-4 py-5`}
+    >
+      <head></head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/lib/site-config.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "Weather",
+  url: "http://localhost:3000/",
+  description: "just a random weather app",
+  ogImage: "",
+  twitterHandle: "@furggoo",
+  lastUpdated: "December 15, 2024",
+};
